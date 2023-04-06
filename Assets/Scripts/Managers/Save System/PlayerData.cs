82b2e523224490b769755ff754a33bf6c9e701f4
Carlos Marques <catralitos11@gmail.com>
2023-04-06 20:45:31 +0100

    Código inicial para o save system feito

---
+++ Assets/Scripts/Managers/Save System/PlayerData.cs
@@ -0,0 +1,31 @@
+﻿using Player;
+using UnityEngine;
+
+namespace Managers.Save_System
+{
+    [System.Serializable]
+    public class PlayerData 
+    {
+        public int maxHealth;
+        public int currentHealth;
+        public float[] playerPosition;
+        public float[] playerRotation;
+        
+        public PlayerData(PlayerEntity playerEntity)
+        {
+            currentHealth = playerEntity.health.currentHealth;
+            maxHealth = playerEntity.health.maxHealth;
+            Transform playerTransform = playerEntity.gameObject.transform;
+            Vector3 playerPos = playerTransform.position;
+            Vector3 playerRot = playerTransform.rotation.eulerAngles;
+            playerPosition = new float[3];
+            playerPosition[0] = playerPos.x;
+            playerPosition[1] = playerPos.y;
+            playerPosition[2] = playerPos.z;
+            playerRotation = new float[3];
+            playerRotation[0] = playerRot.x;
+            playerRotation[1] = playerRot.y;
+            playerRotation[2] = playerRot.z;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/Save System/SaveSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Player;
+using UnityEngine;
+
+namespace Managers.Save_System
+{
+    public static class SaveSystem
+    {
+        public static void SavePlayer(PlayerEntity playerEntity)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            string path = Application.persistentDataPath + "/player.resi";
+            FileStream stream = new FileStream(path, FileMode.Create);
+
+            PlayerData data = new PlayerData(playerEntity);
+        
+            formatter.Serialize(stream, data);
+        
+            stream.Close();
+        
+            Debug.Log("Save file created at " + path);
+        }
+    
+        public static PlayerData LoadPlayer()
+        {
+            string path = Application.persistentDataPath + "/player.resi";
+            if (File.Exists(path))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream stream = new FileStream(path, FileMode.Open);
+                PlayerData data = formatter.Deserialize(stream) as PlayerData;
+                stream.Close();
+                return data;
+            }
+            else
+            {
+                //Debug.LogError("Save file not found at " + path);
+                return null;
+            }
+        }
+    
+        public static void DeletePlayer()
+        {
+            string path = Application.persistentDataPath + "/player.resi";
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+    }
+}
